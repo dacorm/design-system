9bf0b6ce12920e5dfad56f491d751bb1a73d6339
dacorm <stolen.youth@yandex.ru>
2023-03-19 16:58:08 +0700

    feat: build function

---
+++ scss/src/scripts/build.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+const sass = require('node-sass');
+
+const getComponents = () => {
+    let allComponents = [];
+
+    const types = ['atoms', 'molecules', 'organisms'];
+
+    types.forEach((type) => {
+        const allFiles = fs.readdirSync(`src/${type}`).map(file => ({
+            input: `src/${type}/${file}`,
+            output: `src/lib/${file.slice(0, -4) + 'css'}`
+        }));
+
+        allComponents = [
+            ...allComponents,
+            ...allFiles
+        ]
+    });
+
+    return allComponents;
+}
+
+const compile = (route, filename) => {
+    const result = sass.renderSync({
+        data: fs.readFileSync(path.resolve(route)).toString(),
+        outputStyle: 'expanded',
+        outFile: 'global.css',
+        includePaths: [path.resolve('src')]
+    })
+
+    fs.writeFileSync(path.resolve(filename), result.css.toString())
+}
+
+compile('src/global.scss', 'src/lib/global.css');
+
+getComponents().forEach((component) => {
+    compile(component.input, component.output);
+})
